3086f05b101c600d885e0d8a6309692302b32f46
nlb <15624977344@163.com>
2019-06-22 12:49:00 +0800

    's'

--- src/main/java/org/nlb/RabbitMQ/yibu/send.java
+++ src/main/java/org/nlb/RabbitMQ/yibu/send.java
@@ -1,28 +1,45 @@
 package org.nlb.RabbitMQ.yibu;
 
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.client.Connection;
 import org.nlb.RabbitMQ.util.rabbitUtil;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.concurrent.TimeoutException;
 
 public class send {
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         Connection connection = rabbitUtil.getConnection();
         Channel channel = connection.createChannel();
-        channel.queueDeclare("tx",false,false,false,null);
-            channel.confirmSelect();
-
-        for (int i=1;i<10;i++) {
-            String msg = "tx";
-            channel.basicPublish("","tx",null,msg.getBytes());
-        }
-        if(!channel.waitForConfirms()){
-                System.out.println("失败");
-            }else {
-                System.out.println("成功");
+        channel.queueDeclare("yibu",false,false,false,null);
+        channel.confirmSelect();
+        final SortedSet<Long> confirmSet = Collections.synchronizedSortedSet(new TreeSet<Long>());
+        channel.addConfirmListener(new ConfirmListener() {//进行监听
+            public void handleAck(long l, boolean b) throws IOException {
+                //没有问题的
+                if(b){
+                    confirmSet.headSet(l+1).clear();
+                }else {
+                    confirmSet.remove(l);
+                }
             }
+            public void handleNack(long l, boolean b) throws IOException {
+                //有问题的
+                if(b){
+                    confirmSet.headSet(l+1).clear();
+                }else {
+                    confirmSet.remove(l);
+                }
+            }
+        });
+        String msg = "msg";
+
+
 
         channel.close();
         connection.close();
